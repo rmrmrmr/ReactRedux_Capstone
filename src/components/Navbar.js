e5f9561d2e323fe11b24f0ba434ddfd19c9c02a3
rmrmrmr <ricardomartineza77@gmail.com>
2023-01-30 15:01:14 -0600

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-  <header
-    className="flex justify-between items-center border-solid border border-slate-700"
-  >
-    <div
-      className="flex items-center"
+const Navbar = () => {
+  const activeClassName = 'underline';
+  return (
+    <header
+      className="flex justify-between items-center border-solid border-b border-slate-700 mx-10 py-3"
     >
-      <img
-        src="../img/planet.png"
-        alt="PlanetLogo"
-        className="h-16"
-      />
-      <h1
-        className="text-3xl"
+      <div
+        className="flex items-center gap-4"
       >
-        Space Traveler&apos;s Hub
-      </h1>
-    </div>
-    <nav
-      className="flex"
-    >
-      <h2>Rockets</h2>
-      <h2>Missions</h2>
-      <span>|</span>
-      <NavLink to="/">My Profile</NavLink>
-    </nav>
-  </header>
-);
+        <img
+          src="../img/planet.png"
+          alt="PlanetLogo"
+          className="h-16"
+        />
+        <h1
+          className="text-3xl"
+        >
+          Space Traveler&apos;s Hub
+        </h1>
+      </div>
+      <nav
+        className="flex gap-4 text-cyan-600"
+      >
+        <h2>Rockets</h2>
+        <h2>Missions</h2>
+        <span className="text-black">|</span>
+        <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : undefined)}>My Profile</NavLink>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
